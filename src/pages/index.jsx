78f9603ffff8b86c15b4b5545dec6472548b03ef
rts-Liva <U2451012@rts-365.dk>
2025-05-19 14:05:34 +0200

    coming soon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import Fetch from "../components/fetch";
 import Header from "../components/header";
 import Search from "../components/search";
 import '../scss/components/user-popup.scss';
+import '../scss/components/coming-soon.scss';
 
 function HomePage() {
     const [comingSoon, setComingSoon] = useState([]);
@@ -14,7 +15,7 @@ function HomePage() {
             setPopup('welcome');
         }, 3000);
     }, []);
-    
+
     return (
         <>
             <Fetch
@@ -30,6 +31,22 @@ function HomePage() {
                 <p className="welcome__text">Welcome Back,<span className="welcome__text welcome__text--user">username</span></p>
                 <img src="/placeholder.jpg" alt="user-profile" className="welcome__pfp" />
             </div>
+            <main className="home">
+                <section className="coming-soon">
+                    <h2 className="coming-soon__heading">coming soon</h2>
+                    <div className="coming-soon__slider">
+                        {comingSoon.length > 0 ? (
+                            comingSoon.map((movie, index) => (
+                                <article key={index} className="coming-soon__slider__item">
+                                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} poster`} className="coming-soon__slider__item-poster" />
+                                    <h3 className="coming-soon__slider__item-title">{movie.title}</h3>
+                                    <p className="coming-soon__slider__item-release">{movie.release_date}</p>
+                                </article>
+                            ))
+                        ) : <p>Loading...</p>}
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
