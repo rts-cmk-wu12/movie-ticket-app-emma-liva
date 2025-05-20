9a91037ef52eb7fc7cff7002e3cab8343e6dbd7f
rts-Liva <U2451012@rts-365.dk>
2025-05-20 11:55:25 +0200

    add footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,22 @@
+import { BsPersonFill } from "react-icons/bs";
+import { FaBookmark, FaHouse } from "react-icons/fa6";
+import { PiCompassFill } from "react-icons/pi";
+import { Link } from "react-router";
+import '../scss/components/footer.scss';
+
+function Footer({ current }) {
+    return (
+        <footer className="footer">
+            <nav>
+                <ul className="footer-menu">
+                    <li><Link to='/'><FaHouse color={current === 'home' ? '#54a8e5' : '#636882'} className="footer-menu__icon" /></Link></li>
+                    <li><Link to='/explore'><PiCompassFill color={current === 'explore' ? '#54a8e5' : '#636882'} className="footer-menu__icon" /></Link></li>
+                    <li><Link to='/bookmark'><FaBookmark color={current === 'bookmark' ? '#54a8e5' : '#636882'} className="footer-menu__icon" /></Link></li>
+                    <li><Link to='/profile'><BsPersonFill color={current === 'profile' ? '#54a8e5' : '#636882'} className="footer-menu__icon" /></Link></li>
+                </ul>
+            </nav>
+        </footer>
+    );
+}
+
+export default Footer;
