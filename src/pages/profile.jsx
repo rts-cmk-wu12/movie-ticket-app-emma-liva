d2d1829e20932bf8a0d46dc4f215038e2589f6e3
EmmaRosby <U2457534@rts-365.dk>
2025-05-22 13:48:50 +0200

    feat: implement Profile page with user account details and styling

---
+++ src/pages/profile.jsx
@@ -0,0 +1,71 @@
+import { IoIosArrowForward } from "react-icons/io";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import { BiSolidBarChartAlt2 } from "react-icons/bi";
+import { TiUserAdd } from "react-icons/ti";
+import { FaTrash } from "react-icons/fa6";
+import { HiBell } from "react-icons/hi";
+
+import { HiMiniTicket } from "react-icons/hi2";
+import { IoLogOut } from "react-icons/io5";
+
+function Profile() {
+    return (  
+        <>
+        <Header 
+            title="Profile"
+            navigateReturn={false}
+        />
+
+        <main className="profileMain">
+            <div className="profile">
+                <div className="profile__container">
+                    <img src="/placeholder.jpg" alt="Profile-img" />
+                    <p>Username</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+            </div>
+            <section className="profile">
+                <h2>Account</h2>
+                <div className="profile__container">
+                    <div className="profile__container__icon profile__container__icon--dark-blue"><BiSolidBarChartAlt2 /></div>
+                    <p>Personal Data</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+                <div className="profile__container">
+                    <div className="profile__container__icon profile__container__icon--blue"><TiUserAdd /></div>
+                    <p>Email & Payment</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+                <div className="profile__container">
+                    <div className="profile__container__icon profile__container__icon--red"><FaTrash /></div>
+                    <p>Deactivate Account</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+            </section>
+            <section className="profile profile--last">
+                <h2>Privacy & Policy</h2>
+                <div className="profile__container">
+                    <div className="profile__container__icon profile__container__icon--dark-blue"><HiBell /></div>
+                    <p>Notifications</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+                <div className="profile__container">
+                    <div className="profile__container__icon profile__container__icon--blue"><HiMiniTicket /></div>
+                    <p>Your Tickets</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+                <div className="profile__container">
+                    <div className="profile__container__icon profile__container__icon--red"><IoLogOut /></div>
+                    <p>Logout</p>
+                    <button><IoIosArrowForward /></button>
+                </div>
+            </section>
+        </main>
+
+        <Footer current="profile" />    
+        </>
+    );
+}
+
+export default Profile;
--- src/pages/tester.jsx
+++ src/pages/tester.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import Popup from "../components/popup";
-import { RiShieldCheckFill } from "react-icons/ri";
 
 function Tester() {
-    const [showPopup, setShowPopup] = useState(false);
+
 
     return (
         <>
-            <button onClick={() => setShowPopup(true)}>Show Popup</button>
-            <Popup
-                title='Your payment was successful'
-                description='Refund will only be given if the ticket is cancelled 1 hour before the movie starts.'
-                buttonText='See E-Ticket'
-                link='/e-ticket'
-                icon={<RiShieldCheckFill />}
-                show={showPopup}
-            />
+
+
         </>
 
     )
