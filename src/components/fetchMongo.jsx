c1ef0e6188a79f709e30074f16d1926d03967d08
rts-Liva <U2451012@rts-365.dk>
2025-05-22 10:40:11 +0200

    fix url

--- src/components/fetchMongo.jsx
+++ src/components/fetchMongo.jsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react";
 
-function FetchMongo({ setData }) {
+function FetchMongo({ fetchUrl, setData }) {
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`${import.meta.env.VITE_URL}/api/tickets`);
+            const response = await fetch(`${import.meta.env.VITE_URL}${fetchUrl}`);
             const data = await response.json();
 
             setData(data);
         }
 
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+    
     return null;
 }
 
