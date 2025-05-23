8b2b95a69213d0332c7f17364a0b1cea63366bc9
EmmaRosby <U2457534@rts-365.dk>
2025-05-23 10:54:08 +0200

    feat: implement ConfirmPopup component with styling and integration in Tester page

--- src/pages/tester.jsx
+++ src/pages/tester.jsx
@@ -1,15 +1,29 @@
-import LogIn from "../components/logIn";
-import SignUp from "../components/signUp";
+import { useState } from "react";
+import ConfirmPopup from "../components/confirmPopup";
+
 
 function Tester() {
-
+    const [showConfirmPopup, setShowConfirmPopup] = useState(false);
+    function handleConfirm() {
+        console.log("Confirmed!");
+        // Additional confirmation logic here
+    };
 
     return (
         <>
-    <LogIn />
-
-    <SignUp />
-
+        <button 
+            type="button"
+            onClick={() => setShowConfirmPopup(true)}
+        >Tester</button>
+        <ConfirmPopup 
+            title="Test Title"
+            message="This is a test message."
+            cancelBtn="Cancel"
+            confirmBtn="Confirm"
+            show={showConfirmPopup}
+            onCancel={() => setShowConfirmPopup(false)}
+            onConfirm={handleConfirm}
+        />
         </>
 
     )
