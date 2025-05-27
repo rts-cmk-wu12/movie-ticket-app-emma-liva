17cb4b6f024b209eca82cb3e17903fb2fd1d6aa8
rts-Liva <U2451012@rts-365.dk>
2025-05-27 08:50:01 +0200

    prevent random data from re-render

--- src/components/random-date.jsx
+++ src/components/random-date.jsx
@@ -1,5 +1,36 @@
-function RandomDate({ date }) {
-    return <option value={date}>{date}</option>;
+import { useMemo } from "react";
+
+function RandomDate() {
+    const randomDate = useMemo(() => {
+        const currentDate = new Date();
+        const currentMonth = currentDate.getMonth() + 1;
+        const currentDay = currentDate.getDate();
+
+        const dayMax = 10;
+        const dayMin = 1;
+
+        let targetMonth = currentMonth;
+        let targetDay = currentDay + Math.floor(Math.random() * dayMax) + dayMin;
+
+        // Get days in current month
+        const daysInMonth = new Date(currentDate.getFullYear(), currentMonth, 0).getDate();
+
+        // If the target day exceeds current month's days, proceed to the next month
+        if (targetDay > daysInMonth) {
+            targetMonth = currentMonth + 1;
+            // Adjust the day to be the difference (If we're 2 days over, it becomes the 2nd of the next month)
+            targetDay = targetDay - daysInMonth;
+
+            // If the next month exceeds 12, reset to January (Though we never adress what year it is...)
+            if (targetMonth > 12) {
+                targetMonth = 1;
+            }
+        }
+
+        return `${targetDay}/${targetMonth}`;
+    }, [])
+
+    return <option value={randomDate}>{randomDate}</option>;
 }
 
 export default RandomDate;
--- src/pages/select/[title].jsx
+++ src/pages/select/[title].jsx
@@ -17,42 +17,6 @@ function SelectSeat() {
 
     const userId = localStorage.getItem('user');
 
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1;
-    const currentDay = currentDate.getDate();
-
-    const dayMax = 10;
-    const dayMin = 1;
-
-    function createRandomDate() {
-        let targetMonth = currentMonth;
-        let targetDay = currentDay + Math.floor(Math.random() * dayMax) + dayMin;
-
-        // Get days in current month
-        const daysInMonth = new Date(currentDate.getFullYear(), currentMonth, 0).getDate();
-
-        // If the target day exceeds current month's days, proceed to the next month
-        if (targetDay > daysInMonth) {
-            targetMonth = currentMonth + 1;
-            // Adjust the day to be the difference (If we're 2 days over, it becomes the 2nd of the next month)
-            targetDay = targetDay - daysInMonth;
-
-            // If the next month exceeds 12, reset to January (Though we never adress what year it is...)
-            if (targetMonth > 12) {
-                targetMonth = 1;
-            }
-        }
-
-        return `${targetDay}/${targetMonth}`;
-    }
-
-    const hourMax = 22;
-    const hourMin = 8;
-
-    function createRandomTime() {
-        return Math.floor(Math.random() * (hourMax - hourMin + 1)) + hourMin;
-    }
-
     async function handleSubmit(e) {
         e.preventDefault();
         if (userId) {
@@ -109,18 +73,18 @@ function SelectSeat() {
                     <div>
                         <label htmlFor="date" className="select-seats__label">Date</label>
                         <select name="date" id="date" className="select-seats__select">
-                            <RandomDate date={createRandomDate()} />
-                            <RandomDate date={createRandomDate()} />
-                            <RandomDate date={createRandomDate()} />
-                            <RandomDate date={createRandomDate()} />
+                            <RandomDate />
+                            <RandomDate />
+                            <RandomDate />
+                            <RandomDate />
                         </select>
                     </div>
                     <div>
                         <label htmlFor="time" className="select-seats__label">Time</label>
                         <select name="time" id="time" className="select-seats__select">
-                            <RandomTime time={createRandomTime()} />
-                            <RandomTime time={createRandomTime()} />
-                            <RandomTime time={createRandomTime()} />
+                            <RandomTime />
+                            <RandomTime />
+                            <RandomTime />
                         </select>
                     </div>
                     <button type="submit" className="select-seats__btn">checkout</button>
